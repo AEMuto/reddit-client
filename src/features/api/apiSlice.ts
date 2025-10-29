0dfa5fff5f5a7b4a6acd738be4a6ac4ae23fd630
AEMuto <antoine.marseaud@gmail.com>
2025-10-29 18:02:04 +0100

    feat: update getSubreddits query to accept QueryParams for improved flexibility

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -1,14 +1,32 @@
 import type { CommentResponse, PostListResponse, SubredditListResponse } from '@/types/ApiResponses';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+export type QueryParams = 
+  | { count: number; after: string; before?: never }
+  | { count: number; before: string; after?: never }
+  | void;
+
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ 
     baseUrl: import.meta.env.VITE_API_ROOT || 'https://www.reddit.com' 
   }),
   endpoints: (builder) => ({
-    getSubreddits: builder.query<SubredditListResponse, void>({
-      query: () => '/subreddits.json',
+    getSubreddits: builder.query<SubredditListResponse, QueryParams>({
+      query: (params) => {
+        if (!params) return '/subreddits.json';
+        
+        const queryParams = new URLSearchParams();
+        queryParams.set('count', params.count.toString());
+        
+        if (params.after) {
+          queryParams.set('after', params.after);
+        } else if (params.before) {
+          queryParams.set('before', params.before);
+        }
+        
+        return `/subreddits.json?${queryParams.toString()}`;
+      },
     }),
     getSearch: builder.query<PostListResponse, string>({
       query: (searchTerm) => `/search.json?q=${encodeURIComponent(searchTerm)}`,
