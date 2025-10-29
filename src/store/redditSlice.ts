38395f8bd444d3d2d7c689d462c817cf5a1426dc
AEMuto <antoine.marseaud@gmail.com>
2025-10-29 18:01:41 +0100

    feat: add redditSlice for managing search term and selected subreddit state

---
+++ src/store/redditSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  searchTerm: "",
+  selectedSubreddit: "",
+};
+
+export type AppState = typeof initialState;
+
+export const redditSlice = createSlice({
+  name: "reddit",
+  initialState,
+  reducers: {
+    setSearchTerm(state, action) {
+      state.searchTerm = action.payload;
+    },
+    setSelectedSubreddit(state, action) {
+      state.selectedSubreddit = action.payload;
+    },
+  },
+  selectors: {
+    selectSearchTerm: (state: AppState) => state.searchTerm,
+    selectSelectedSubreddit: (state: AppState) => state.selectedSubreddit,
+  },
+});
+
+export const { setSearchTerm, setSelectedSubreddit } = redditSlice.actions;
+export const { selectSearchTerm, selectSelectedSubreddit } = redditSlice.selectors;
