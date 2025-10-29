8a6955f365019321dd2a9afa541330e049e10c9d
AEMuto <antoine.marseaud@gmail.com>
2025-10-29 18:02:31 +0100

    feat: refactor Home component to utilize Redux state for search term and selected subreddit

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,11 +1,18 @@
-import { Header } from "../header/Header";
+import { useAppSelector } from "@/store/hooks";
 import { Subreddits } from "../subreddits/Subreddits";
+import { selectSearchTerm, selectSelectedSubreddit } from "@/store/redditSlice";
+import { useEffect } from "react";
 
 export const Home = () => {
+  const searchTerm = useAppSelector(selectSearchTerm);
+  const selectedSubreddit = useAppSelector(selectSelectedSubreddit);
+  useEffect(() => {
+    console.log("Search Term:", searchTerm);
+    console.log("Selected Subreddit:", selectedSubreddit);
+  }, [searchTerm, selectedSubreddit]);
   return (
-    <>
-      <Header />
+    <main className="p-4 bg-gray-300">
       <Subreddits />
-    </>
+    </main>
   );
 };
